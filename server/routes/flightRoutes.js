96e1bbaaac8218c640e18fe17a992a58ac3376a6
shankarjatin <shankarjatin1005@gmail.com>
2025-01-05 22:12:47 +0530

    flight routes

---
+++ server/routes/flightRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const flightController = require('../controllers/flightController');
+const authenticateUser = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Protect the flight search and flight price routes
+router.post('/flights', authenticateUser, flightController.searchFlights);
+router.get('/prices', authenticateUser, flightController.getFlightPrices);
+
+module.exports = router;
